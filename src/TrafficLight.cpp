3ccfe820a6c084b093c0f1928288ecc47523b580
Roma Barge <l.roma.barge@tue.nl>
2020-08-27 09:22:10 +0200

    FP.5.b waitForGreen implementation

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -50,6 +50,15 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+
+    while (1)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        auto color = _queue.receive();
+        if(color == green){
+            return;
+        }
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
